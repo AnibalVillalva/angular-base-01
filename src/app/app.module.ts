49dadb0c18455b0cd5512dfef37febab1a330495
Anibal Villalva <avillalva@conexia.com>
2022-06-07 10:45:52 -0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component'
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { HeroesModules } from './heroes/heroes.modules';
+import { CounterModules } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModules,
+    CounterModules,
   ],
   providers: [],
   bootstrap: [AppComponent]

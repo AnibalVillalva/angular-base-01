b9000f6a8116be87c496e6f67b1935bc130f47b3
Anibal Villalva <avillalva@conexia.com>
2022-06-08 10:10:26 -0300

    New Modules and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModules } from './heroes/heroes.modules';
 import { CounterModules } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CounterModules } from './counter/counter.module';
     BrowserModule,
     HeroesModules,
     CounterModules,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]

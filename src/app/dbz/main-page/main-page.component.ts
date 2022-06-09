1a3a0c3273ac61b5cda908d885af9e61cdf0b0ed
Anibal Villalva <avillalva@conexia.com>
2022-06-08 22:46:21 -0300

    ngModel 2wayDataModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Character {
+  name: string,
+  power: number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  newCharacter: Character = {
+    name:"P",
+    power:0
   }
 
+  addCharacter() {
+    console.log(this.newCharacter);
+  }
 }
